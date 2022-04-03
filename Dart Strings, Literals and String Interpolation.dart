e85b444a5e1b106603ba07385d967737ce046e40
Rohit Singh Rathore <96055471+rohit511@users.noreply.github.com>
2022-04-03 14:36:20 +0530

    Add files via upload

---
+++ Dart Strings, Literals and String Interpolation.dart
@@ -0,0 +1,15 @@
+void main(){
+  //literals
+  String s1="it is a very easy";
+  print(s1);
+  String s2 ="hellow"+" world";
+  print(s2);
+  //String interpolatin
+  var name="rohit";
+var message="my name is $name";
+print(message);
+int l=20;
+int b =34;
+print("sum of $l and $b is ${l+b}");
+print("area of $l and $b is ${l*b}");
+}
---
+++ varibles.dart
@@ -0,0 +1,12 @@
+void main(){
+  int a=6;
+  var age =10;// age is varible
+  print(a);
+  print(age);
+  print(44==44);
+  double exponents=2.567e5;
+  print(exponents);
+  var t;
+  print(t);
+   
+} 
